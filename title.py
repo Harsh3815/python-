2d03246d2e0aa13da30bf430a45859b0be12dad3
Harsh3815 <harshkatrodiya336@gmail.com>
2025-08-28 08:36:14 +0530

    Add files via upload

---
+++ title.py
@@ -0,0 +1,8 @@
+import pylab as plt
+x=[1,2,3,4]
+y=[3,8,1,10]
+plt.plot(x,y,ls='dotted')
+plt.title('example')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
---
+++ xlint_ylimit.py
@@ -0,0 +1,7 @@
+import pylab as plt
+x=[1,2,3,1]
+y=[3,8,7,5]
+plt.plot(x,y)
+plt.xlim(0.6)
+plt.ylim(0.13)
+plt.show()
